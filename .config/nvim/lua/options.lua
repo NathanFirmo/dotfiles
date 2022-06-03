41587a72353a91712a704a00e030895109673415
NathanFirmo <nathanssfirmo@gmail.com>
2022-06-03 00:56:30 -0300

    feat: add initial configs to lua

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,30 @@
+local set = vim.opt
+
+vim.g.mapleader = ' ' -- Usar a tecla de espaço como tecla líder
+set.clipboard:append('unnamedplus') -- Copiar e colar texto da área de tranferência
+set.nu = true -- Usar numeração absoluta nas linhas
+set.tabstop = 2        -- Show existing tab with 4 spaces width
+set.softtabstop = 2    -- Show existing tab with 4 spaces width
+set.shiftwidth = 2    -- When indenting with '>', use 4 spaces width
+set.expandtab = true        -- On pressing tab, insert 4 spaces
+set.smarttab  = true        -- insert tabs on the start of a line according to shiftwidth
+set.smartindent = true      -- Automatically inserts one extra level of indentation in some cases
+set.hidden = true           -- Hides the current buffer when a new file is openned
+set.incsearch = true        -- Incremental search
+set.ignorecase = true       -- Ingore case in search
+set.smartcase = true        -- Consider case if there is a upper case character
+set.scrolloff = 8      -- Minimum number of lines to keep above and below the cursor
+-- set.colorcolumn = 160  -- Draws a line at the given line to keep aware of the line size
+set.signcolumn= 'yes'   -- Add a column on the left. Useful for linting
+set.cmdheight = 2      -- Give more space for displaying messages
+set.updatetime = 100   -- Time in miliseconds to consider the changes
+set.encoding = 'UTF-8'   -- The encoding should be utf-8 to activate the font icons
+set.backup = false         -- No backup files
+set.writebackup = false    -- No backup files
+set.splitright = true       -- Create the vertical splits to the right
+set.splitbelow = true       -- Create the horizontal splits below
+set.autoread = true         -- Update vim after file update from outside
+set.mouse = 'a'          -- Enable mouse support
+-- filetype on          -- Detect and set the filetype option and trigger the FileType Event
+-- filetype plugin on   -- Load the plugin file for the file type, if any
+-- filetype indent on   -- Load the indent file for the file type, if any
