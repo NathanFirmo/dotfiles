676b503cc5270463570add7a37bffdebb738d926
NathanFirmo <nathanssfirmo@gmail.com>
2023-01-16 23:17:26 -0300

    feat: add prisma to treesitter

--- .config/nvim/lua/treesitter-config.lua
+++ .config/nvim/lua/treesitter-config.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or 'all'
-  ensure_installed = { 'javascript', 'typescript', 'tsx', 'json', 'yaml', 'html', 'css', 'scss' },
+  ensure_installed = { 'javascript', 'typescript', 'tsx', 'json', 'yaml', 'html', 'css', 'prisma' },
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = true,
   highlight = {
