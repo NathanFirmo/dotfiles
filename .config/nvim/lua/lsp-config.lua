8b4c44bef4780490bb9e7a38c1d1c52b41e2b61d
NathanFirmo <nathanssfirmo@gmail.com>
2023-01-19 08:45:22 -0300

    feat: add yaml language server

--- .config/nvim/lua/lsp-config.lua
+++ .config/nvim/lua/lsp-config.lua
@@ -61,6 +61,20 @@ lspconfig.jsonls.setup {
   capabilities = capabilities,
 }
 
+lspconfig.yamlls.setup {
+  capabilities = capabilities,
+  settings = {
+    yaml = {
+      schemas = {
+        ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+        ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] =  "/*.k8s.yaml" ,
+        ["http://json.schemastore.org/kustomization"] =  "kustomization.yaml" ,
+        ["https://raw.githubusercontent.com/GoogleContainerTools/skaffold/master/docs/content/en/schemas/v2beta8.json"] =  "skaffold.yaml",
+      },
+    },
+  }
+}
+
 lspconfig.clangd.setup {
   capabilities = capabilities,
 }
