689b7b60617f1bb535e96f97ef4f4587f5670df6
NathanFirmo <nathanssfirmo@gmail.com>
2023-02-03 11:17:24 -0300

    chore: remove unnused scripts

--- scripts/music.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-status=$(playerctl status)
-
-if [[ $status == 'Paused' ]]; then
-  echo $(playerctl metadata --format ' {{ title }} - {{ artist }}')
-else
-  echo $(playerctl metadata --format ' {{ title }} - {{ artist }}')
-fi
--- try-on-docker.sh
+++
@@ -1,8 +0,0 @@
-docker run -w /root -it --rm alpine:edge sh -uelic '
-    apk add git nodejs neovim ripgrep alpine-sdk --update
-    git clone https://github.com/NathanFirmo/dotfiles          
-    cp -r dotfiles/.config .config
-    git clone --depth 1 https://github.com/wbthomason/packer.nvim .local/share/nvim/site/pack/packer/start/packer.nvim
-    nvim --headless -c "autocmd User PackerComplete quitall" -c "PackerSync"
-    nvim
-    '
